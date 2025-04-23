edc85b8c720880071651cf30d3b144ccf9796821
David <david.sut13@gmail.com>
2025-04-23 21:34:46 +0300

    added register page placeholder

--- movie_rec/src/router/index.js
+++ movie_rec/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-  ],
+    {
+      path: '/',
+      name: 'register',
+      component: RegisterView,
+    },
+  ]
 })
 
 export default router
